8b8053e05702929c8b13c4165e1a1ac2ad5eadfe
V3 Office <rjlafazan@gmail.com>
2019-03-13 12:26:29 -0700

    conditional virtual box download

--- downloads.sh
+++ downloads.sh
@@ -1,5 +1,16 @@
 
 ## Downloads
+
+### Install virtual box yes or no
+while true; do
+    read -p "Do you have virtual box already?" yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) brew cask install virtualbox;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
 ### Virtual box image
 wget https://cdn.amazonlinux.com/os-images/2.0.20190228/virtualbox/amzn2-virtualbox-2.0.20190228-x86_64.xfs.gpt.vdi
 
