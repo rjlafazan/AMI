0da17f60c0ead612b75fcf7c4b26490258397323
V3 Office <rjlafazan@gmail.com>
2019-03-13 11:50:47 -0700

    simple shell script to download resources

---
+++ downloads.sh
@@ -0,0 +1,7 @@
+
+## Downloads
+### Virtual box image
+wget https://cdn.amazonlinux.com/os-images/2.0.20190228/virtualbox/amzn2-virtualbox-2.0.20190228-x86_64.xfs.gpt.vdi
+
+### Seeds iso
+wget https://cdn.amazonlinux.com/os-images/2.0.20190228/Seed.iso
